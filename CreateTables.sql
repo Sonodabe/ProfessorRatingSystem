1e6dbceae1387f70a21e0ac17bed6d52298263d3
db4soundman <db4soundman@gmail.com>
2015-04-28 10:22:13 -0400

    Ported the insert statements into our sql file, so the database is updated now.

--- CreateTables.sql
+++ CreateTables.sql
@@ -54,5 +54,32 @@ UName    varchar NOT NULL,
 primary key(UName)
 );
 
+INSERT INTO University values ('Miami University');
+INSERT INTO University values('University of Kentucky');
+INSERT INTO University values('Xavier University');
+
+INSERT INTO Professor (PName, ResearchArea, Bio, YearsWorked) 
+VALUES ('Mallory Ross', 'Artificial Intelligence', 'Graduated from WVU in 1991', 12);
+INSERT INTO Professor (PName, ResearchArea, Bio, YearsWorked) 
+VALUES ('Eric Smith', 'Big Data', 'Received his PhD from Harvard', 19);
+INSERT INTO Professor (PName, ResearchArea, Bio, YearsWorked) 
+VALUES ('Max Rowell', 'Computer Architecture', 'Researching at Miami University', 8);
+
+INSERT INTO Student (SName, Major, Username, University)
+VALUES ('Doug Blase', 'Sports Management', 'blasedd', 'University of Kentucky');
+INSERT INTO Student (SName, Major, Username, University)
+VALUES ('Samantha Wolf', 'Mathematics', 'wolfsp', 'Xavier University');
+INSERT INTO Student (SName, Major, Username, University)
+VALUES ('Brandon Sonoda', 'Psychology', 'sonodabe', 'Miami University');
+
+INSERT INTO Course values('CSE 626', 'Computing in an Imaginary World', 'University of Kentucky');
+INSERT INTO Course values('ISA 412', 'Advanced System Analytics and Stuff', 'Miami University');
+
+INSERT INTO Teaches values('CSE 626', 2);
+INSERT INTO Teaches values('ISA 412', 1);
+
+INSERT INTO Review values(1, 2, 2015, 'CSE 626', 'Spring', 3, 1, 5, 2, 2, 'Hard class');
+INSERT INTO Review values(3, 1, 2012, 'ISA 412', 'Fall', 2, 2, 4, 2, 1, 'Not my favorite class');
+
 
 
