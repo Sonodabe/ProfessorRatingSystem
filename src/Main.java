baacabad57e7b5508c55fad3637893e713a93feb
dizzyupthegirl <wolfsp@miamioh.edu>
2015-04-27 22:20:53 -0400

    Which side appears
    Added a pop up window that controls which side of the application you
    see

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
 import graphics.PRSFrame;
-import javax.swing.JFrame;
+import javax.swing.*;
 
 /**
  * Implements a driver of the program
  * 
- * @author Brandon Sonoda
+ * @author Brandon Sonoda and Samantha Wolf
  *
  */
 public class Main {
 	public static void main(String[] args) {
-		PRSFrame frame = new PRSFrame(false);
+		Object[] options = {"User",
+                "Admin"};
+		int n = JOptionPane.showOptionDialog(null,
+			    "Welcome! Are you a user or administrator?",
+			    "Welcome!",
+			    JOptionPane.YES_NO_CANCEL_OPTION,
+			    JOptionPane.QUESTION_MESSAGE,
+			    null,
+			    options,
+			    options[0]);
+		boolean admin= n==1 ? true:false;		
+		PRSFrame frame = new PRSFrame(admin);
 		frame.setVisible(true);
 		frame.setBounds(100, 100, 600, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
